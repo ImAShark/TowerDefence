933ebc23c8403cfde517549087e207fb8c7d788e
killersshadow <32833605+killersshadow@users.noreply.github.com>
2018-11-01 09:02:54 +0100

    camera Contolls

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    public float movementspeed = 1;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,11 +13,30 @@ public class CameraMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Controlls();
+
+    }
 
     private void Controlls()
     {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movementspeed, 0, 0);
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movementspeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, movementspeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -movementspeed, 0);
+        }
     }
 }
